6f9e0e7a4251357ab9b4491f785f77fd2eb31620
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-11 13:33:44 -0600

    Mostrando mensajes y animaciones en componente de busqueda

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -13,13 +13,16 @@ export const SearchPage = () => {
   // console.log(q);
   const heroes = getHeroesByName(q);
 
+  const showSearch = (q.length === 0);
+  const showError  = (q.length > 0) && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
     searchText: q
   });
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if (searchText.trim().length === 0) return;
+    // if (searchText.trim().length === 0) return;
     // console.log({ searchText });
     navigate(`?q=${ searchText }`)
   }
@@ -53,15 +56,25 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
+          {/* {
+            ( q === '' )
+              ? <div className="alert alert-primary">Search a hero</div>
+              : (heroes.length === 0 ) && 
+              <div className="alert alert-danger">No hero with <b>{ q }</b> </div>
+          } */}
+
+          <div 
+            className="alert alert-primary animate__animated animate__fadeIn" 
+            style={{ display: !showSearch ? 'none' : '' }}>
             Search a hero
           </div>
 
-          <div className="alert alert-danger">
-            No hero with <b>{ q }</b>
+          <div 
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? '' : 'none' }}>
+            No hero with <b>{ q }</b> 
           </div>
 
-          {/* <HeroCard /> */}
           {
             heroes.map(hero => (
               <HeroCard
